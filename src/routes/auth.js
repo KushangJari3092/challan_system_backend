c86a6a4d6360444c71d24af62f26c33d17e4d987
KushangJari3092 <kushangjari3092@gmail.com>
2023-04-09 15:35:51 +0530

    final commit

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,31 +11,28 @@ require('../db/conn')
 const Admin = require('../models/admin');
 const Police = require('../models/police');
 const superAdmin = require('../models/superAdmin');
+const User = require('../models/users');
+const Challan = require('../models/challans');
 
 router.get('/', (req, res) => {
     res.send("hello home")
 })
 
 router.post('/register', async (req, res) => {
-    // console.log("hi1")
     const { name, id, bday, email, adhaar, password, mobile, gender, address, registerfor } = req.body;
     try {
         if (registerfor === 'Admin') {
-            // console.log("hi2")
             const ae = await Admin.findOne({ aEmail: email })
             const aa = await Admin.findOne({ aAdhaar: adhaar })
             const am = await Admin.findOne({ aMobile: mobile })
             const ai = await Admin.findOne({ aId: id })
             if (ae || aa || am || ai) {
-                // console.log('a :>> ', ae);
                 return res.status(422).json({ err: "already exist" })
             }
             console.log("hi3")
             const admin = new Admin({ aName: name, aId: id, aBday: bday, aEmail: email, aAdhaar: adhaar, aPassword: password, aMobile: mobile, aGender: gender, aAddress: address })
-            // console.log("hi4")
             //calling pre method before save
             const savedAdmin = await admin.save()
-            // console.log("hi5")
             // console.log('Admin :>> ', savedAdmin);
             return res.status(200).json({ success: "registration successful" })
         }
@@ -71,7 +68,7 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ err: 'invalid details' })
         }
         else if (superadmin) {
-            console.log('superadmin :>> ', superadmin);
+            // console.log('superadmin :>> ', superadmin);
             if (superadmin.password != password)
                 return res.status(400).json({ err: "invalid details " })
             else {
@@ -83,7 +80,7 @@ router.post('/login', async (req, res) => {
             }
         }
         else if (admin) {
-            console.log('admin :>> ', admin);
+            // console.log('admin :>> ', admin);
             const isMatch = await bcrypt.compare(password, admin.aHashPassword);
             if (!isMatch)
                 return res.status(400).json({ err: "invalid details of admin " })
@@ -108,15 +105,37 @@ router.post('/login', async (req, res) => {
                 const token = await p.generateAuthToken();
                 // console.log('police token :>> ', token);
                 res.cookie("policeToken", token, {
-                    expires: new Date(Date.now() + 9999999999),
+                    expires: new Date(Date.now() + 3600 * 24 * 365),
                 })
                 res.cookie('person', 'police', {
-                    expires: new Date(Date.now() + 9999999999),
+                    expires: new Date(Date.now() + 3600 * 24 * 365),
                 })
                 return res.json({ ...p._doc, success: "login successful" })
             }
         }
+    }
+    else if (person === 'user') {
+        const user = await User.findOne({ vehicleNo: uname });
 
+        if (!user) {
+
+            return res.status(400).json({ err: 'vehicle does not exists' })
+        }
+        if (user) {
+            const token = await user.generateAuthToken();
+
+            const challan = await Challan.find({ vehicleNum: uname });
+            console.log('challan :>> ', challan);
+            res.cookie("userToken", token, {
+                expires: new Date(Date.now() + 3600 * 24 * 365),
+            })
+            res.cookie('person', 'user', {
+                expires: new Date(Date.now() + 3600 * 24 * 365),
+            })
+
+            return res.json({ ...challan, success: "login successful" })
+
+        }
     }
 
 })
@@ -136,7 +155,7 @@ router.get('/logout', (req, res) => {
     }
     else if (req.cookies.person === 'user') {
         res.clearCookie('userToken');
-        res.clearCookie('user')
+        res.clearCookie('person')
     }
     return res.json({ loggedOut: req.cookies.person + " logged out" })
 })
@@ -160,6 +179,46 @@ router.get('/getData', async (req, res) => {
         }
     } catch (err) {
         console.log('err in adminInfo auth :>> ', err);
+    }
+})
+router.get('/getChallans', async (req, res) => {
+    try {
+        const challans = await Challan.find();
+        // console.log('challans :>> ', challans);
+        res.send(challans);
+    } catch (err) {
+        console.log('err in get challan auth :>> ', err);
+    }
+})
+
+router.post('/submitChallan', async (req, res) => {
+    try {
+        const { pname, pid, offense, place, plateNum, vehicleType, date, time, dueDate } = req.body;
+        console.log('req.cody :>> ', req.body);
+        const citizen = await User.findOne({ "vehicleNo": { "$in": [plateNum] } })
+        console.log('citizen :>> ', citizen);
+        let amount = 0;
+        if (offense === 'Violation of road regulations') { amount = 100; }
+        else if (offense === 'Driving without License') { amount = 150; }
+        else if (offense === 'Over Speeding') { amount = 200; }
+        else if (offense === 'Drunken Driving') { amount = 500; }
+        else if (offense === 'offense related to accident') { amount = 400; }
+        else if (offense === 'Violation of Parking regulations') { amount = 100; }
+        console.log("hii")
+        console.log('citizen.... :>> ', citizen.name);
+        console.log('citizen.... :>> ', citizen.licenseNo);
+        console.log('citizen.... :>> ', citizen.contact);
+        console.log('type :>> ', vehicleType);
+        const challan = new Challan({
+            policeName: pname, policeId: pid, vehicleType: vehicleType, vehicleNum: plateNum, offense, place, time, date, amount,
+            user: citizen.name, licenseNo: citizen.licenseNo, mobile: citizen.contact, email: citizen.email, dueDate, status: 'pending'
+        })
+
+        await challan.save();
+        return res.status(200).json({ success: "challan submitted successfully" })
+    }
+    catch (err) {
+        return res.json({ err: "challan is not submitted" })
     }
 })
 module.exports = router;
