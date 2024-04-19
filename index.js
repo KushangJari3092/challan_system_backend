4db75c3fe29c943ae18cfdcfffe8e1b2247ae846
KushangJari3092 <86768697+KushangJari3092@users.noreply.github.com>
2024-04-19 16:25:09 +0530

    Update index.js *

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ exp.use(express.json())
 
 const cookieParser = require('cookie-parser');
 exp.use(cookieParser());
-exp.use(cors({
-    origin: "https://662238ba251134ae9b8ac870--singular-sprinkles-a2c4bf.netlify.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
-
-
-require('./src/db/conn');
-exp.use(require('./src/routes/auth.js'))
+// exp.use(cors({
+//     origin: "https://662238ba251134ae9b8ac870--singular-sprinkles-a2c4bf.netlify.app",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }));
 
 exp.use((req, res, next) => {
   res.header({"Access-Control-Allow-Origin": "*"});
   next();
 }) 
+
+require('./src/db/conn');
+exp.use(require('./src/routes/auth.js'))
 
 exp.listen(7100, () => {
     console.log("running on port 7100")
